8da25eca3011699bcf574f5313f011e310e38479
rober <rober@RTZ>
2023-07-27 01:58:57 +0300

    Created simple min_Max values, and index pulling

---
+++ MAX_MIN_VALUES.m
@@ -0,0 +1,25 @@
+clc, clearvars,close all;
+
+x = linspace(0,5);
+
+y = (-(x-3).^2) + 10;
+
+MaxVal = max(y);
+
+[MaxVal,Index] = max(y)
+
+x_MaxVal = x(Index)
+
+disp("INDEX OF THE MAX VALUE OF Y, AND INDEX OF X")
+
+MinVal = min(y) ;
+
+[MinVal,Index] = min(y) 
+
+x_MinVal = x(Index)
+disp("INDEX OF THE MIN VALUE OF Y, AND INDEX OF X")
+
+
+
+
+
